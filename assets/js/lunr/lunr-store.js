896aebbee98e2e6f92b25bcecf8974daa551c58f
Javier Caride Ulloa <javier.caride@gmail.com>
2022-05-07 20:35:47 +0200

    Site version on 2022-05-07

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "¡Hola mundo! (de nuevo)",
+        "excerpt":"Hace unos años (en pleno boom de los blogs donde si no tenías uno no eras nadie) mantenía mi blog personal donde principalmente contaba aquellas cosas que iba aprendiendo durante mi carrera profesional. Pero el tiempo necesario para mantenerlo actualizado, el coste de mantener un hosting, la ardua tarea de...","categories": ["general"],
+        "tags": [],
+        "url": "/general/2020/04/15/hellow-world-again.html",
+        "teaser": null
+      },{
+        "title": "Salir de mi zona de confort: PHP a NodeJS y Typescript",
+        "excerpt":"Llevo desarrollando software de forma profesional desde 1999, año en el que descubrí PHP. Recuerdo muy bien que lo descubrí de casualidad en una revista en papel (sí, papel) sobre Linux que me compraba por aquel entonces. Hablaban de la versión 3.0 del lenguaje y de como añadirle algunas librerías...","categories": ["desarrollo","typescript"],
+        "tags": ["typescript","php","evolución"],
+        "url": "/desarrollo/typescript/2020/10/13/saliendo-zona-de-confort.html",
+        "teaser": null
+      },{
+        "title": "Empezando mi ruta hacia Typescript: instalando Node.js",
+        "excerpt":"Como con todas las tecnologías a las que uno llega por primera vez, lo más complejo de adentrarse en el mundo de Node.js es comprender todo lo que necesitas para empezar. Los que venimos del mundo PHP sabemos que la configuración más estandar para empezar es símplemente un servidor HTTP...","categories": ["desarrollo","typescript"],
+        "tags": [],
+        "url": "/desarrollo/typescript/2021/02/01/empezando-mi-ruta-hacia-typescript-instalando-node-js.html",
+        "teaser": null
+      },{
+        "title": "Creando un esqueleto de API básico con Typescript y Koa - Parte 1 ",
+        "excerpt":"Una vez tuve Node.js instalado, el siguiente objetivo que me marqué fue montar un esqueleto básico para poder hacer una pequeña API REST usando Typescript. Leí que se podía trabajar con Node.js nativo pero para no reinventar la rueda y acelerar la velocidad de desarrollo, tenía que elegir un framework...","categories": ["desarrollo","typescript"],
+        "tags": [],
+        "url": "/desarrollo/typescript/2021/06/19/creando-esqueleto-api-typescript-koa-parte-i.html",
+        "teaser": null
+      }]
